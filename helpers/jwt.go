d4f19e1713a22a5bee8aaeb01987c2843d19a1e1
ikhlasrama16 <ikhlasrama16@gmail.com>
2025-05-08 09:23:37 +0700

    jwt & validator error

---
+++ helpers/jwt.go
@@ -0,0 +1,25 @@
+package helpers
+
+import (
+	"example/hello/config"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte(config.GetEnv("JWT_SECRET", "secret_key"))
+
+func GenerateToken(username string) string {
+
+	expirationTime := time.Now().Add(60 * time.Minute)
+
+	claims := &jwt.RegisteredClaims{
+		Subject:   username,
+		ExpiresAt: jwt.NewNumericDate(expirationTime),
+	}
+
+	token, _ := jwt.NewWithClaims(jwt.SigningMethodPS256, claims).SignedString(jwtKey)
+
+	return token
+
+}
